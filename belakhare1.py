81560adf48d6e7269014ff8addcad63f4d2f25f9
Saman833 <144067555+Saman833@users.noreply.github.com>
2023-12-05 12:26:37 +0330

    Add files via upload

---
+++ belakhare1.py
@@ -0,0 +1,15 @@
+import  pygame
+class MyClass:
+    number = 5
+    string = "Hello!"
+    def __init__(self, location):
+        self.location_x = location[0]
+        self.location_y = location[1]
+    def change_location(self, amount):
+        self.location_x += amount
+        self.location_y += amount
+        return  self.location_x, self.location_y
+    def increment_number(self):
+        MyClass.number += 1   
+    def pending_functionality(self):
+       pass 
