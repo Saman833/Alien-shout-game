9b431777139b1437d70e13b55bcbe53c1571a8ea
Saman833 <144067555+Saman833@users.noreply.github.com>
2023-12-05 12:22:39 +0330

    Add files via upload

---
+++ belakhare.py
@@ -0,0 +1,12 @@
+import  pygame
+class MyClass:
+    number = 5
+    string = "Hello!"
+    def __init__(self, location):
+        self.location_x = location[0]
+        self.location_y = location[1]
+        def change_location(self, amount):
+        self.location_x += amount
+        self.location_y += amount
+        return  self.location_x, self.location_y
+        
