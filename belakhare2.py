333fbdc7497aaa4693b818d4c538ba70a8967db4
Saman833 <144067555+Saman833@users.noreply.github.com>
2023-12-05 14:19:52 +0330

    Add files via upload

---
+++ belakhare2.py
@@ -0,0 +1,53 @@
+#import  pygame
+class MyClass:
+    number = 5
+    string = "Hello!"
+    def __init__(self,location):
+        self.location_x = location[0]
+        self.location_y = location[1]
+    def change_location(self, amount):
+        self.location_x += amount
+        self.location_y += amount
+    def increment_number(self):
+        MyClass.number += 1   
+    def pending_functionality(self):
+       pass 
+class Alien:
+    total_aliens_created=0
+    xl=0
+    def __init__(self,location):
+        self.health=3
+        Alien.total_aliens_created+=1
+        self.x_coordinate=location[0]
+        self.y_coordinate=location[1]
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+             
+
