0cf3d125e69f9dc2757b16cf63a8d92fddfdc803
Saman833 <144067555+Saman833@users.noreply.github.com>
2023-12-05 12:25:07 +0330

    Add files via upload

--- belakhare.py
+++ belakhare.py
@@ -5,8 +5,9 @@ class MyClass:
     def __init__(self, location):
         self.location_x = location[0]
         self.location_y = location[1]
-        def change_location(self, amount):
+    def change_location(self, amount):
         self.location_x += amount
         self.location_y += amount
         return  self.location_x, self.location_y
-        
+    def increment_number(self):
+        MyClass.number += 1    
